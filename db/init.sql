05165a88dc8248c16859361bc7ffdccd45f450ec
super <artemmarkelov2008@yandex.ru>
2025-03-12 12:35:14 +0300

    create database and connecting for it

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+create table dates (
+id serial primary key unique,
+date timestamp unique
+);
+
+create table rooms (
+date_id int,
+room_number int,
+count_of_windows int,
+foreign key (date_id) references dates(id)
+);
+
+create table lights (
+date_id int,
+floor_number int,
+light_number int,
+is_on bool,
+foreign key (date_id) references dates(id)
+);
---
+++ our_requests.py
@@ -0,0 +1,15 @@
+import json
+import psycopg2
+
+def get_db_connection() -> psycopg2.extensions.connection:
+    with open("config.json") as f:
+        conf = json.load(f)
+    DATABASE = conf["DATABASE"]
+    USER = conf["USER"]
+    PASSWORD = conf["PASSWORD"]
+    HOST = conf["HOST"]
+    PORT = conf["PORT"]
+    conn = psycopg2.connect(
+        dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
+    )
+    return conn
