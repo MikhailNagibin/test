4e31af82cf62818c3f23f1af52ca5b7630234082
super <artemmarkelov2008@yandex.ru>
2025-03-12 13:08:54 +0300

    create database and connecting for it

--- our_requests.py
+++ our_requests.py
@@ -14,3 +14,26 @@ def get_db_connection() -> psycopg2.extensions.connection:
         dbname=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT
     )
     return conn
+
+
+def get_all_days() -> dict:
+    return requests.get('http://127.0.0.1:5000/date').json()
+
+
+def information_by_day(date: str) -> dict:
+    day, month, year = date.split('-')
+    return requests.get('http://127.0.0.1:5000', {'day': day,'month': month, 'year': year}).json()
+
+
+def get_date_id(cur: psycopg2.extensions.cursor, date: int) -> int:
+    cur.execute("select id from dates where date = ?", (date, ))
+    ans = cur.fetchall()
+    if ans:
+        return ans[0]
+    return -1
+
+
+def insert_days_in_db(conn: psycopg2.extensions.connection, data: dict):
+    cur = conn.cursor()
+    print(data['date']['data'])
+    cur.execute("insert into dates(date) values %s", (data['date']['data'], ))
